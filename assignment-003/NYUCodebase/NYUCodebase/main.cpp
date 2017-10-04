fb32b86076d8c4f93c00ace7b7917b940972055e
wecsam <david@davidtsai.net78.net>
2017-10-04 01:45:11 -0400

    Added the loading of the sprite sheet as a texture in main()

--- assignment-003/NYUCodebase/NYUCodebase/main.cpp
+++ assignment-003/NYUCodebase/NYUCodebase/main.cpp
@@ -1,35 +1,70 @@
+#include <iostream>
 #ifdef _WINDOWS
+	#define RESOURCE_FOLDER ""
 	#include <GL/glew.h>
+#else
+	#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
 #endif
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "ShaderProgram.h"
 #include "Dimensions.h"
 
-#ifdef _WINDOWS
-	#define RESOURCE_FOLDER ""
-#else
-	#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
-#endif
+// From assignment 1
+GLuint LoadTexture(const char *filePath) {
+	int w, h, comp;
+	unsigned char* image = stbi_load(filePath, &w, &h, &comp, STBI_rgb_alpha);
+	if (image == NULL) {
+		std::cout << "Unable to load image: " << filePath << std::endl;
+		exit(1);
+	}
+	GLuint result;
+	glGenTextures(1, &result);
+	glBindTexture(GL_TEXTURE_2D, result);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	stbi_image_free(image);
+	return result;
+}
 
 SDL_Window* displayWindow;
 
 int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	displayWindow = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
-	#ifdef _WINDOWS
-		glewInit();
-	#endif
+#ifdef _WINDOWS
+	glewInit();
+#endif
+
+	// Viewport setup
+	glViewport(0, 0, WIDTH, HEIGHT);
+	Matrix projectionMatrix;
+	projectionMatrix.SetOrthoProjection(-ORTHO_X_BOUND, ORTHO_X_BOUND, -ORTHO_Y_BOUND, ORTHO_Y_BOUND, -1.0f, 1.0f);
+	Matrix modelviewMatrix;
+	ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+	glUseProgram(program.programID);
+	program.SetProjectionMatrix(projectionMatrix);
+	program.SetModelviewMatrix(modelviewMatrix);
+
+	// Load textures
+	GLuint spriteSheet = LoadTexture(RESOURCE_FOLDER"Images/sprites.png");
 
 	SDL_Event event;
 	bool done = false;
 	while (!done) {
 		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+			switch (event.type) {
+			case SDL_QUIT:
+			case SDL_WINDOWEVENT_CLOSE:
 				done = true;
+				break;
 			}
 		}
 		glClear(GL_COLOR_BUFFER_BIT);
