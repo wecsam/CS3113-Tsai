826d6bf11d296b67694c859fd5ed40be687a0030
David <david@wecsam.com>
2017-12-12 00:38:59 -0500

    Moved window size to Dimensions.h

---
+++ final-project/NYUCodebase/NYUCodebase/Dimensions.h
@@ -0,0 +1,3 @@
+#pragma once
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
--- final-project/NYUCodebase/NYUCodebase/main.cpp
+++ final-project/NYUCodebase/NYUCodebase/main.cpp
@@ -4,7 +4,7 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
-
+#include "Dimensions.h"
 #ifdef _WINDOWS
 	#define RESOURCE_FOLDER ""
 #else
@@ -16,7 +16,7 @@ SDL_Window* displayWindow;
 int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	displayWindow = SDL_CreateWindow("Levels", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
 #ifdef _WINDOWS
